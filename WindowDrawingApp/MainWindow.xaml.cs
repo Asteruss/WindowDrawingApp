e8f6562344ca7e0d0cff2914426238edc4b1b6b1
Asteruss <artyom.yurchenko2222@mail.ru>
2026-07-06 00:55:20 +0300

    Добавлен MVVM и MainViewModel вывод рамы окна

--- WindowDrawingApp/MainWindow.xaml.cs
+++ WindowDrawingApp/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WindowDrawingApp.ViewModels;
 
 namespace WindowDrawingApp
 {
@@ -19,6 +20,7 @@ namespace WindowDrawingApp
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ WindowDrawingApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WindowDrawingApp.Models;
+
+namespace WindowDrawingApp.ViewModels;
+
+public class MainViewModel : NotifyPropertyChanged
+{
+    private Picture _currentPicture;
+    public Picture CurrentPicture { get => _currentPicture; set=>SetProperty(ref _currentPicture, value);  }
+
+    private WindowNode _selectedNode;
+    public WindowNode SelectedNode { get => _selectedNode; set => SetProperty(ref _selectedNode, value); }
+    private RelayCommand _createPicture;
+    public RelayCommand CreatePicture
+    {
+        get => _createPicture ??= new(obj =>
+        {
+            if (CurrentPicture != null)
+            {
+                var res = MessageBox.Show("Вы уверены? Текущий чертеж будет потерян.", "Предупреждение", MessageBoxButton.YesNo);
+                if (res != MessageBoxResult.Yes) return;
+            }
+
+            CurrentPicture = new Picture(800, 1000);
+        });
+    }
+}
